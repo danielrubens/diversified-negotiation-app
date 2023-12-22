1f0584ae766c00011352527cdb8441457d2f660f
daniel <danielrubensdaniel@gmail.com>
2023-12-22 16:44:52 -0300

    increment scan count

--- app/routes/app.$id.scan.jsx
+++ app/routes/app.$id.scan.jsx
@@ -1,6 +1,8 @@
+import { redirect } from "@remix-run/node";
 import invariant from "tiny-invariant";
 import db from "../db.server";
 
+import { getDestinationUrl } from "../models/QRCode.server";
 
 export const loader = async ({ params }) => {
   invariant(params.id, "Could not find QR code destination");
@@ -9,4 +11,11 @@ export const loader = async ({ params }) => {
   const qrCode = await db.qRCode.findFirst({ where: { id } });
 
   invariant(qrCode, "Could not find QR code destination");
+
+  await db.qRCode.update({
+    where: { id },
+    data: { scans: { increment: 1 } },
+  });
+
+  return redirect(getDestinationUrl(qrCode));
 }
